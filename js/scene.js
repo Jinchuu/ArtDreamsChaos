def4eed02d5a66fec0af6086c3f40dde9b390f50
Jinchuu <61342203+Jinchuu@users.noreply.github.com>
2022-06-08 02:05:50 -0300

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,4 +1,4 @@
-var scene = new THREE.scene();
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 camera.position.z = 5;
@@ -8,7 +8,7 @@ renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: green});
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 

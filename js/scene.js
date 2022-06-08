7313123f3ed26c16533b0f9d761d029e0965c594
Jinchuu <61342203+Jinchuu@users.noreply.github.com>
2022-06-08 01:40:00 -0300

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,5 @@
 var scene = new THREE.scene();
-var camera = new THREE.perspectiveCamera(75, window.innerWidth, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 camera.position.z = 5;
 
